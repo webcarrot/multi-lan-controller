9355371cce7e6e817ea47f34d349abd10d7a00b8
Wojciech Wierchoła <wojciech.wierchola@doctrin.se>
2021-05-27 08:08:52 +0200

    fix build

--- src/admin/routes/dashboard/prepare.ts
+++ src/admin/routes/dashboard/prepare.ts
@@ -4,5 +4,9 @@ const getDefault = <T>({ default: d }: { default: T }): T => d;
 
 export const prepare: Prepare = ({ sort }) =>
   sort
-    ? import("./sort").then(getDefault)
-    : import("./component").then(getDefault);
+    ? import(/* webpackChunkName: "routes/dashboard/sort" */ "./sort").then(
+        getDefault
+      )
+    : import(
+        /* webpackChunkName: "routes/dashboard/component" */ "./component"
+      ).then(getDefault);
